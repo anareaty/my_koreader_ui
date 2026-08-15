9bf8647c4b2c2c70b6bd224a82591ee51430d065
anareaty <reatymain@gmail.com>
2026-08-15 13:34:39 +0400

    change name

--- _meta.lua
+++ _meta.lua
@@ -1,9 +1,9 @@
 
 
 return {
-    name        = "simpleui",
-    fullname    = "Simple UI",
+    name        = "my_koreader_ui",
+    fullname    = "My UI",
     description = [[A simple UI for KOReader]],
-    version     = "1.5.0",
-    author      = "Doctor Hetfield",
+    version     = "1.0.0",
+    author      = "anareaty",
 }
